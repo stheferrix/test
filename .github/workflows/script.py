3c2a717f96bbcf721cae5ed5fd94671debd2edf4
Sthefania Ferri <stheferri@gmail.com>
2024-08-19 15:27:58 -0300

    Update script.py

--- .github/workflows/script.py
+++ .github/workflows/script.py
@@ -10,7 +10,7 @@ public_key = os.environ['PUBLIC_KEY']
 
 print('VALOR =', new_secret_value )
 
-encoded_value = base64.urlsafe_b64encode(new_secret_value.encode()).decode()
+encoded_value = base64.urlsafe_b64encode(new_secret_value.encode('utf-8')).decode()
 
 url = f"https://api.github.com/repos/{os.environ['REPO']}/actions/secrets/{os.environ['SECRET_NAME']}"
 
@@ -22,11 +22,11 @@ headers = {
 from cryptography.fernet import Fernet
 import json
 
-fernet = Fernet(public_key.encode())
-encrypted_value = fernet.encrypt(encoded_value.encode())
+fernet = Fernet(public_key.encode('utf-8'))
+encrypted_value = fernet.encrypt(encoded_value.encode('utf-8'))
 
 data = {
-    'encrypted_value': encrypted_value.decode(),
+    'encrypted_value': encrypted_value.decode('utf-8'),
     'key_id': key_id
 }
 
