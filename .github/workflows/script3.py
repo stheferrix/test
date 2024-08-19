fba12a7e70a05d8e4c00e2e735f7cb1c0d56767e
Sthefania Ferri <stheferri@gmail.com>
2024-08-19 19:27:17 -0300

    Update script3.py

--- .github/workflows/script3.py
+++ .github/workflows/script3.py
@@ -10,17 +10,8 @@ GITHUB_TOKEN = os.environ['GITHUB_TOKEN']
 REPO = os.environ['REPO']
 SECRET_NAME = os.environ['SECRET_NAME']
 SECRET_VALUE = os.environ['NEW_SECRET_VALUE']
-
-# Função para obter a chave pública
-def get_public_key():
-    url = f'https://api.github.com/repos/{REPO}/actions/secrets/public-key'
-    headers = {
-        'Authorization': f'token {GITHUB_TOKEN}',
-        'Accept': 'application/vnd.github.v3+json',
-    }
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()  # Levanta um erro se a requisição falhar
-    return response.json()
+PUBLIC_KEY = os.environ['PUBLIC_KEY']
+KEY_ID = os.environ['KEY_ID']
 
 # Função para criptografar o segredo
 def encrypt_secret(secret_value, public_key):
@@ -33,7 +24,7 @@ def encrypt_secret(secret_value, public_key):
     return base64.b64encode(encrypted_value).decode()
 
 # Função para atualizar o segredo no GitHub
-def update_secret(public_key_info, encrypted_value):
+def update_secret(encrypted_value):
     url = f'https://api.github.com/repos/{REPO}/actions/secrets/{SECRET_NAME}'
     headers = {
         'Authorization': f'token {GITHUB_TOKEN}',
@@ -41,7 +32,7 @@ def update_secret(public_key_info, encrypted_value):
     }
     data = {
         'encrypted_value': encrypted_value,
-        'key_id': public_key_info['key_id'],
+        'key_id': KEY_ID,
     }
     response = requests.put(url, headers=headers, data=json.dumps(data))
     response.raise_for_status()  # Levanta um erro se a requisição falhar
@@ -49,12 +40,11 @@ def update_secret(public_key_info, encrypted_value):
 
 # Execução
 if __name__ == '__main__':
-    # Obter a chave pública
-    public_key_info = get_public_key()
-    public_key = public_key_info['key']
-    
     # Criptografar o valor do segredo
-    encrypted_value = encrypt_secret(SECRET_VALUE, public_key)
+    encrypted_value = encrypt_secret(SECRET_VALUE, PUBLIC_KEY)
+    
+    # Atualizar o segredo no GitHub
+    update_secret(encrypted_value)
     
     # Atualizar o segredo no GitHub
     update_secret(public_key_info, encrypted_value)
